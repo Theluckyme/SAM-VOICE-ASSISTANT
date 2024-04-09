f4857ae1c18522604e27077f0048adfd984e07d6
Subham Kumar  Rana <92314926+Theluckyme@users.noreply.github.com>
2024-04-09 23:26:54 +0530

    Update saveoutput.py

--- assistant/saveoutput.py
+++ assistant/saveoutput.py
@@ -1,6 +1,1 @@
-import os
-# Function to clear the output.txt file
-def clear_output_file():
-    if os.path.exists("output.txt"):
-        os.remove("output.txt")
-        print("Previous messages cleared from output.txt.")
+
